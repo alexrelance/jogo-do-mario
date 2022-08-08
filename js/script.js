a2908252da47fdb4f7824d7813456f7583aa94a5
alexrelance <alexrelance@gmail.com>
2022-08-07 23:13:39 -0300

    adicionando acoes dos botoes no JS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+    const mario = document.querySelector('.mario');
+    const pipe = document.querySelector('.pipe');
+
+    const jump = () => {
+        mario.classList.add('jump');
+
+        setTimeout(() => {
+
+            mario.classList.remove('jump');  
+
+        }, 500);
+    }
+    
+
+document.addEventListener('keydown', jump);
